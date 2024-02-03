c0b34870dd57073ddec22446ffe13b07e911c6c3
majamanda <majamandasintha@gmail.com>
2024-02-03 17:55:19 +0200

    take me up lesson 4

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,13 +2,26 @@
 import './App.css';
 
 function App() {
+
+  const title = "Welcome to the new blog";
+  const likes = 58;
+  const link = "https://www.google.com";
+
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Content</h1>
+        <h1>{ title }</h1>
+        <p>Liked { likes } times </p>
+        <p> { [1,2,3,4,7] } </p>
+        <p>Rondom num X 10: {Math.random()*10} </p>
+
+        <a href={link}>Visit Google</a>
+
       </div>
     </div>
   );
+
 }
 
 export default App;
