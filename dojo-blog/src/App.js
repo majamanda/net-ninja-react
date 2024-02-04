fee956341d9f0c47e102e9d6f640a1d4106d2f80
majamanda <majamandasintha@gmail.com>
2024-02-04 17:45:48 +0200

    added branch lesson5

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,23 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from 'Navbar';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-
-  const title = "Welcome to the new blog";
-  const likes = 58;
+ 
+  // const title = "Welcome to the new blog";
+  // const likes = 58;
   const link = "https://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <Navbar />
-
-        <h1>{ title }</h1>
-        <p>Liked { likes } times </p>
-        <p> { [1,2,3,4,7] } </p>
-        <p>Rondom num X 10: {Math.random()*10} </p>
-
+        <Home />
         <a href={link}>Visit Google</a>
 
       </div>
---
+++ dojo-blog/src/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return ( 
+        <div className="home">
+            <h2>Welcome to the Blog!</h2>
+        </div>
+     );
+}
+ 
+export default Home;
