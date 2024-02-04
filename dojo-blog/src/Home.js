4936a3d4af79576ddbfb09a9dd166675fd1478b4
majamanda <majamandasintha@gmail.com>
2024-02-04 23:50:44 +0200

    click event, check out the event object/parameter lesson7

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = () => {
+        console.log('Hello Ninjas');
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target, e);
+    }
+
     return ( 
         <div className="home">
             <h2>Welcome to the Blog!</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={ (e) => {
+                handleClickAgain('Ntha', e)
+            } }>Click me again</button>
         </div>
      );
 }
