04f1a81fc21c049b42228cbe16d3468c5bbf3de8
majamanda <majamandasintha@gmail.com>
2024-02-07 11:16:50 +0200

    after lesson10

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,21 +2,29 @@ import { useState } from "react";
 
 const Home = () => {
 
-    const [name, setName] = useState('Jane');
-    const [age, setAge] = useState(20);
+    // const [name, setName] = useState('Jane');
+    // const [age, setAge] = useState(20);
 
-    const handleClick = () => {
-        setName('Joe');
-        setAge(29);
-    }
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+    ])
 
     return ( 
         <div className="home">
-            <h2>Welcome to the Blog!</h2>
-            <p>{ name } is { age } years old.</p>
-            <button onClick={handleClick}>Click Me</button>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id}>
+                    <h2 className="blog-title">
+                        {blog.title}
+                    </h2>
+                    <p className="author">
+                        {blog.author}
+                    </p>
+                </div>
+            ))}
         </div>
-     );
+    );
 }
  
 export default Home;
