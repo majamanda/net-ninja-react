126f0cf3d12a75bffce16a088870e981191fe6d2
majamanda <majamandasintha@gmail.com>
2024-02-11 15:46:22 +0200

    after lesson 15

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -9,16 +9,25 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ])
 
+    const [name, setName] = useState('John');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
         setBlogs(newBlogs);
     };
 
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title={"All Blogs!"} handleDelete={handleDelete}/>
             <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title={"Mario's Blogs"} handleDelete={handleDelete}/>
-            {/* <BlogList /> */}
+            <button onClick={() => name === 'John'?  setName('Joe') : setName('John')} >
+                {name}
+            </button>
         </div>
     );
 }
