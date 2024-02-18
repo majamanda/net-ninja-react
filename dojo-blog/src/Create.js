a82844939748e9faaf5311cb375d6f3a27b949fd
majamanda <majamandasintha@gmail.com>
2024-02-18 22:15:47 +0200

    in lesson 27 Controlled Inputs

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,9 +1,38 @@
+import { useState } from "react";
+
 const Create = () => {
-  return ( 
+
+  const [title, setTitle] = useState('title');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('');
+
+  return (
     <div className="create">
       <h2>Add a new blog</h2>
+      <form action="" method="post">
+        <label htmlFor="title">Blog title:</label>
+        <input type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <label htmlFor="title">Blog body:</label>
+        <textarea name="content">
+        </textarea>
+
+        <label htmlFor="title">Blog author:</label>
+        <select>
+          <option value="ben">Ben</option>
+          <option value="Joe">Joe</option>
+          <option value="Mary">Mary</option>
+        </select>
+        <button>Add blog</button>
+        <p>{ title }</p>
+        <p>{ body }</p>
+      </form>
     </div>
-   );
+  );
 }
- 
+
 export default Create;
