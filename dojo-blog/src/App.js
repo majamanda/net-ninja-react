39ecf2e6af6adf1c03a1ef5dcddc0fa94a74e6af
majamanda <majamandasintha@gmail.com>
2024-02-18 19:14:46 +0200

    after lesson23

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -2,7 +2,7 @@
 // import './App.css';
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Create from './Create';
 
 function App() {
