7e793afc7fcc89f3a4c1dcea81f044540d61697b
majamanda <majamandasintha@gmail.com>
2024-02-18 21:02:55 +0200

    after  lesson26

--- dojo-blog/src/BlogDetails.js
+++ dojo-blog/src/BlogDetails.js
@@ -1,13 +1,25 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
+import { useState } from "react";
 
 const BlogDetails = () => {
   const { id } = useParams();
-  
-  return ( 
+  const [URL, setURL] = useState('http://localhost:8000/blogs/' + id)
+  const { data: blog, error, isPending } = useFetch(URL);
+
+  return (
     <div className="blog-details">
-      <h2>Blog details - { id }</h2>
+      {isPending && <div> Loading...</div>}
+      {error && <div> {error}</div>}
+      {blog && (
+        <article>
+          <h2> {blog.title}</h2>
+          <div>{blog.body}</div>
+          <p>Written by {blog.author}</p>
+        </article>
+      )}
     </div>
-   );
+  );
 }
- 
+
 export default BlogDetails;
