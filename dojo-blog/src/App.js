44ec38971f106beb10581ede691361d2f991702b
majamanda <majamandasintha@gmail.com>
2024-02-19 12:57:24 +0200

    after lesson 32 finale!!

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -5,6 +5,7 @@ import Home from './Home';
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import NotFound from './NotFound';
 
 function App() {
 
@@ -27,6 +28,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
